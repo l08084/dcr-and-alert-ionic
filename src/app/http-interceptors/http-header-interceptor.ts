f989af988dfa84a7ebc23a40fda085e1d831d520
l08084 <l08084.1989@gmail.com>
2021-03-07 18:31:35 +0900

    update

---
+++ src/app/http-interceptors/http-header-interceptor.ts
@@ -0,0 +1,12 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HttpHeaderInterceptor implements HttpInterceptor {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler):
+    Observable<HttpEvent<any>> {
+    return next.handle(req);
+  }
+}
