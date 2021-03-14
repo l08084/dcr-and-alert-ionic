cd6729390f7a03f522a660275aa8abf42d644e66
l08084 <l08084.1989@gmail.com>
2021-03-14 16:45:47 +0900

    update dcr.js

--- scripts/dcr.js
+++ scripts/dcr.js
@@ -1,9 +1,24 @@
 "use strict";
 
 /*
- * add dark canary release http header
+ * ダークカナリアリリース用のHTTPヘッダーを追加する。
  */
-const env = process.env;
-console.log(env.RELEASE_TYPE);
+const env = process.env,
+  releaseModel = env.RELEASE_MODEL | "",
+  fs = require("fs");
 
-console.log("hello");
+const addReleaseModelHeader = (releaseModel) => {
+  if (releaseModel === "") {
+    return;
+  }
+  const httpInterceptorFilePath =
+    "./src/app/http-interceptors/http-header-interceptor.ts";
+  let httpInterceptorFile = fs.readFileSync(httpInterceptorFilePath, "utf-8");
+  httpInterceptorFile = httpInterceptorFile.replace(
+    /\/\* \{RELEASE_MODEL\} \*\//,
+    `.set('x-relase-model', '${releaseModel}')`
+  );
+  fs.writeFileSync(httpInterceptorFilePath, httpInterceptorFile);
+};
+
+addReleaseModelHeader(releaseModel);
