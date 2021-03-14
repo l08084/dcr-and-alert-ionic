c77e0b5a9e282bd37cb9e3569e0687af9b918902
l08084 <l08084.1989@gmail.com>
2021-03-14 17:10:32 +0900

    add dcr.js

--- scripts/dcr.js
+++ scripts/dcr.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
 /*
  * ダークカナリアリリース用のHTTPヘッダーを追加する。
  */
 const env = process.env,
-  releaseModel = env.RELEASE_MODEL || "",
-  fs = require("fs");
+  releaseModel = env.RELEASE_MODEL || '',
+  fs = require('fs');
 
 const addReleaseModelHeader = (releaseModel) => {
-  if (releaseModel === "") {
+  if (releaseModel === '') {
     return;
   }
   const httpInterceptorFilePath =
-    "./src/app/http-interceptors/http-header-interceptor.ts";
-  let httpInterceptorFile = fs.readFileSync(httpInterceptorFilePath, "utf-8");
+    './src/app/http-interceptors/http-header-interceptor.ts';
+  let httpInterceptorFile = fs.readFileSync(httpInterceptorFilePath, 'utf-8');
   httpInterceptorFile = httpInterceptorFile.replace(
     /\/\* \{RELEASE_MODEL\} \*\//,
     `.set('x-relase-model', '${releaseModel}')`
