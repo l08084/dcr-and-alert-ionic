78261433e917c5d342c16fe53d5b1bffb73930be
l08084 <l08084.1989@gmail.com>
2021-03-21 17:43:32 +0900

    update

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { ExclusiveAlertService } from '../services/exclusive-alert.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  constructor(private exclusiveAlertService: ExclusiveAlertService) {}
 
-  constructor() {}
+  public async openAlertA(): Promise<void> {
+    await this.exclusiveAlertService.presentAAlert();
+  }
 
+  public async openAlertB(): Promise<void> {
+    await this.exclusiveAlertService.presentBAlert();
+  }
 }
