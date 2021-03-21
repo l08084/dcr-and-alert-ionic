f63287afe858279b5fff5552f0be73a64b997061
l08084 <l08084.1989@gmail.com>
2021-03-21 17:31:14 +0900

    add present alert method

--- src/app/services/exclusive-alert.service.ts
+++ src/app/services/exclusive-alert.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ExclusiveAlertService {
+  private aAlert: HTMLIonAlertElement;
+  private bAlert: HTMLIonAlertElement;
 
-  constructor() { }
+  constructor(private alertController: AlertController) {}
+
+  public async presentAAlert(): Promise<void> {
+    this.aAlert = await this.alertController.create({
+      header: 'アラートA',
+      message: 'アラートAです。アラートBとは同時に表示されません',
+    });
+    await this.aAlert.present();
+  }
+
+  public async presentBAlert(): Promise<void> {
+    this.bAlert = await this.alertController.create({
+      header: 'アラートB',
+      message: 'アラートBです。アラートAとは同時に表示されません',
+    });
+    await this.bAlert.present();
+  }
 }
