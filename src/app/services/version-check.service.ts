e55b8892ad4a95e18331ece8597d24260baf6898
l08084 <l08084.1989@gmail.com>
2021-03-28 15:30:38 +0900

    add angularFireDatabase 

--- src/app/services/version-check.service.ts
+++ src/app/services/version-check.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 @Injectable({
   providedIn: 'root',
 })
 export class VersionCheckService {
   private readonly appVersion = '1.0.0';
 
-  constructor() {}
+  constructor(private db: AngularFireDatabase) {}
 
   public initSetting(): void {
     console.log(this.appVersion);
